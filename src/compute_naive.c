68182533098a99d492522e6f2cfc7d2af2e1dab7
tranviviana <tranvivianaedu@gmail.com>
2024-04-18 15:11:02 -0700

    need to check but written code

--- src/compute_naive.c
+++ src/compute_naive.c
@@ -9,39 +9,56 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
 
   int32_t temp;
   for(uint32_t i = 0; i < total_items / 2; i++) {
-      temp = b_matrix[i];
-      b_matrix[i] = b_matrix[total_items - i - 1];
-      b_matrix[total_items - i - 1] = temp;
+      temp = (b_matrix->data)[i];
+      (b_matrix->data)[i] = (b_matrix->data)[total_items - i - 1];
+      (b_matrix-> data)[total_items - i - 1] = temp;
   }
   //now we malloc space for the output_matrix
   uint32_t output_rows = (a_matrix->rows) / (b_matrix -> rows);
   uint32_t output_cols = (a_matrix->cols) / (b_matrix -> cols);
   int32_t *output = (int32_t*) malloc (output_rows + output_cols) * sizeof(int32_t);
+  if (output == null) {
+      return -1;
+  }
   uint32_t output_numitems = output_rows * out_cols;
   
   //now we element wise multiple
-  uint32_t counter = 0;
-  while (((counter + b_matrix -> rows) < output_numitems) && (counter + b_matrix -> cols) < output_numitems) {
-  uint32_t row_counter = 0;
-  uint32_t col_counter = 0;
-  while 
-
-
-
-
-  col_counter = 0;
-  row_counter ++;
+  uint32_t counter = 0; //starting location for the a matrix
+  uint32_t output_counter = 0; //location for output matrix
+  while ((counter + (b_matrix -> cols) < (a_matrix->cols)) && (counter + (b_matrix -> rows) < (a_matrix->rows))) {
+    //check that the block is within the constraints of the right corner
+  while (((counter - 1 + (b_matrix -> cols)) % a_matrix->cols) != 0 ) {
+      //check that the block is within the constraints of the right side
+    uint32_t sum = blockwise(counter, b_matrix, a_matrix, total_items) 
+    output[output_counter] = sum;
+    output_counter += 1;
+    counter += 1;
   }
-  
-      
-  
-  
-    
+  counter += (b_matrix-> cols);
+  }
+  //put the matrix we made into output matrix slot
+  output_matrix = &output;
+  return 0;
+}
+int blockwise(uint32_t start_location, matrix_t b_matrix, matrix_t a_matrix, int total_items) {
+    *int32_t b_array = b_matrix->data;
+    *int32_t a_array = a_matrix->data;
+    int b_cols = b_matrix->cols;
+    int counter = 0;
+    int col_counter = 0
+    uint32_t sum = 0
+    while (counter < total_items) {
+        if (col_counter % b_cols) {
+            start_location = start_location + b_cols;
+            col_counter = 0;
+        }
+        sum += b_array[counter] * a_array[start_location];
+        counter++;
+        start_location++;
+        col_counter++;
+    }
+    return sum;
 
-
-
-
-  return -1;
 }
 
 // Executes a task
