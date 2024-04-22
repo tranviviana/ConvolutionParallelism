3f6907787e01f7899a0655d9dbd88a0696b56fb3
tranviviana <tranvivianaedu@gmail.com>
2024-04-22 02:18:57 -0700

    like dont delete bro

--- src/compute_optimized_mpi.c
+++ src/compute_optimized_mpi.c
@@ -7,7 +7,81 @@
 int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   // TODO: convolve matrix a and matrix b, and store the resulting matrix in
   // output_matrix
+  int32_t *b_data = b_matrix->data;
+  int32_t *a_data = a_matrix->data;
+  int b_rows = b_matrix->rows;
+  int b_cols = b_matrix->cols;
+  int a_rows = a_matrix->rows;
+  int a_cols = a_matrix->cols;
+  uint32_t total_items = (b_rows) * (b_cols);
+  int32_t *b_reversed = malloc(sizeof(int32_t) * total_items);
+  //now we malloc space for the output_matrix
+  uint32_t output_rows = (a_rows) - (b_rows) + 1;
+  uint32_t output_cols = (a_cols) - (b_cols) + 1;
+  int32_t *output = (int32_t*) malloc((output_rows * output_cols) * sizeof(int32_t));
+  matrix_t *omatrix = (matrix_t *) malloc(sizeof(matrix_t));
+  //set *output_matrix to be a point to resulting matrix where matrix is allocated
+  *output_matrix = omatrix;
+  omatrix->rows = output_rows;
+  omatrix->cols = output_cols;
+  omatrix->data = output;
+    //location in a is the counter
+  if (output == NULL || output_matrix == NULL) {
+      return -1;
+  }
+  //flip b matrix matrix flip should be right
+  #pragma omp parallel for
+  for(int i = 0; i < total_items/4 *4; i+=4) {
+      *(b_reversed + i) = *(b_data + total_items - i - 1);
+      *(b_reversed + i + 1) = *(b_data + total_items - i - 2);
+      *(b_reversed + i + 2) = *(b_data + total_items - i - 3);
+      *(b_reversed + i + 3) = *(b_data + total_items - i - 4);
+  }
+  //tail case
+  #pragma omp parallel for
+  for(int i = total_items/4 * 4; i < total_items; i++) {
+      *(b_reversed + i) = *(b_data + total_items - i - 1);
+  }
+  b_matrix->data = b_reversed;
+  free(b_data);
+  b_data = b_matrix->data;
+  for (int row = 0; row < output_rows; row++) {
+    #pragma omp parallel for
+    for (int col = 0; col < output_cols; col++) {
+          __m256i sum_groups;
+          __m256i a_vector;
+          __m256i b_vector;
+          int32_t sum = 0;
+          __m256i a_vector2;
+          __m256i b_vector2;
+          sum_groups = _mm256_setzero_si256();
 
+          int32_t temp_arr[8];
+
+          for (int i = 0; i < b_rows; i++) {
+              //sum_groups = _mm256_setzero_si256();
+            for(int j = 0; j < b_cols/16 * 16; j+=16) {
+                a_vector = _mm256_loadu_si256((__m256i *)(a_data + (i * a_cols) + (row * a_cols) + (j + col)));
+                a_vector2 = _mm256_loadu_si256((__m256i *)(a_data + (i * a_cols) + (row * a_cols) + (j + 8 + col)));
+
+                b_vector = _mm256_loadu_si256((__m256i*)(b_data + (i * b_cols) + j));
+                b_vector2 = _mm256_loadu_si256((__m256i*)(b_data + (i * b_cols) + j + 8));
+
+                sum_groups = _mm256_add_epi32(sum_groups, _mm256_mullo_epi32(a_vector, b_vector));
+                sum_groups = _mm256_add_epi32(sum_groups, _mm256_mullo_epi32(a_vector2, b_vector2));
+
+          }
+            //tail case
+            for (int c = b_cols/16 * 16; c < b_cols; c++) {
+                sum += b_data[i * b_cols + c] * a_data[(i * a_cols) + (row * a_cols) + c + col];
+            }
+
+      }
+            _mm256_storeu_si256((__m256i*)temp_arr, sum_groups);
+            sum += temp_arr[0] + temp_arr[1] + temp_arr[2] + temp_arr[3] + temp_arr[4] + temp_arr[5] + temp_arr[6] + temp_arr[7];
+             *(output + (row * output_cols) + col) = sum;
+    }
+}
   return -1;
 }
 
--- src/coordinator_mpi.c
+++ src/coordinator_mpi.c
@@ -1,6 +1,9 @@
 #include <mpi.h>
-
+#include <omp.h>
+#include <x86intrin.h>
+#include <immintrin.h>
 #include "coordinator.h"
+#include "compute.h"
 
 #define READY 0
 #define NEW_TASK 1
@@ -12,6 +15,55 @@ int main(int argc, char *argv[]) {
     printf("Usage: %s [path_to_task_list]\n", argv[0]);
     return -1;
   }
+  int numTasks;
+  //= atoi(argv[1]);
+  task_t **tasks;
+  if (read_tasks(argv[1], &numTasks, &tasks)) {
+          printf("error reading task list from %s\n", argv[1]);
+          return -1;
+          }
 
-  // TODO: implement Open MPI coordinator
+  MPI_Init(&argc, &argv);
+  int procID, totalProcs;
+  MPI_Comm_size(MPI_COMM_WORLD, &totalProcs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &procID);
+  if(procID == 0) {
+      int nextTask = 0;
+      MPI_Status status;
+      int32_t message;
+      while (nextTask < numTasks) {
+      MPI_Recv(&message, 1, MPI_INT32_T, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+      int sourceProc = status.MPI_SOURCE;
+      message = nextTask;
+      MPI_Send(&message, 1, MPI_INT32_T, sourceProc, 0, MPI_COMM_WORLD);
+      nextTask++;
+      }
+      //wait till finish
+      for (int i = 0; i < totalProcs - 1; i++) {
+      // wait for message
+      MPI_Recv(&message, 1, MPI_INT32_T, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+      int sourceProc = status.MPI_SOURCE;
+      message = TERMINATE;
+      MPI_Send(&message, 1, MPI_INT32_T, sourceProc, 0, MPI_COMM_WORLD);
+      }
+      }
+   else {
+      int32_t message;
+      while(true) {
+      message = READY;
+      MPI_Send(&message, 1, MPI_INT32_T, 0, 0, MPI_COMM_WORLD);
+      //receiving message
+      MPI_Recv(&message, 1, MPI_INT32_T, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      if (message == TERMINATE) break;
+    if (execute_task(tasks[message])) {
+      printf("Task %d failed\n", message);
+      return -1;
+    }
+      //execute_task(tasks[message]);
+      //free(tasks[message]->path);
+      //free(tasks[message]);
+      
+      }
+  }
+  MPI_Finalize();
 }
